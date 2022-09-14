c105039a54e722089ffd5f07fa209843c0758e85
Romy Schleicher <rschleic@3-B-12.42heilbronn.de>
2022-09-14 18:07:43 +0200

    brainfart

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -4,10 +4,10 @@ int	main(){
 
 /****************<array><random access>*************************/
 	std::array<int, 5> intArr = {1, 2, 3, 4, 5};
+	int	value = 3;
 	//without {} random values, with {} everything 0
 	try{
 
-		int	value = 3;
 		::easyfind(intArr, value);
 		value = 33;
 		::easyfind(intArr, value);
@@ -38,13 +38,11 @@ int	main(){
 /****************<list><bidirectional iterator>*************************/
 //can't acces with easyfind - [] anymore - becasue bidirectional
 	std::list<int> intL;
-		// intL.push_back(11);
-		// intL.push_back(22);
-		// intL.push_back(33);
+		intL.push_back(11);
+		intL.push_back(22);
+		intL.push_back(33);
 	try{
-		int	value = 33;
 		easyfind2(intL, value);
-		//warum kann ich hier nicht einfach ne Zahl eingeben???
 	}
 	catch(std::exception &ex){
 		std::cout << ex.what() << ":	number not an element of the array" << std::endl;
